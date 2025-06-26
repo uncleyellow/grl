a70daea3a3328171ab3b7b18b7a4bfe3e3cd490b
uncleyellowdk@ratraco.vn <uncleyellowdk@ratraco.vn>
2025-06-26 16:29:12 +0700

    sua env thanh cong 103.7

--- grl-web/src/app/shared/enviroment.ts
+++ grl-web/src/app/shared/enviroment.ts
@@ -9,6 +9,7 @@ export const environment = {
     api: {
       url: 'https://beexcelgreenline.onrender.com/api',
       // url: 'http://localhost:3000/api',
+      // url: 'https://103.7.41.213/api',
 
       headers: {
         'Content-Type': 'application/json',
--- grl-web/src/environments/environment.prod.ts
+++ grl-web/src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
     api: {
         url: 'https://beexcelgreenline.onrender.com/api',
         // url: 'http://localhost:3000/api',
+        // url: 'https://103.7.41.213/api',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
     }
 };
-  
