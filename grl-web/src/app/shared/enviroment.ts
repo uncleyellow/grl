44d2f064412f292a3ffd09c0a7e9424dbd22cb21
uncleyellowdk@ratraco.vn <uncleyellowdk@ratraco.vn>
2025-06-27 08:36:18 +0700

    suawr env27.06

--- grl-web/src/app/shared/enviroment.ts
+++ grl-web/src/app/shared/enviroment.ts
@@ -7,9 +7,9 @@
 export const environment = {
     production: true,
     api: {
-      url: 'https://beexcelgreenline.onrender.com/api',
+      // url: 'https://beexcelgreenline.onrender.com/api',
       // url: 'http://localhost:3000/api',
-      // url: 'https://103.7.41.213/api',
+      url: 'https://nrgreenlines.com.vn/api',
 
       headers: {
         'Content-Type': 'application/json',
